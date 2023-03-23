65e7a9f25e7a95b83641db0767185de00cbb0b0b
Popov-Ruslan <ruslan.popov87@gmail.com>
2023-03-23 22:37:18 +0300

    1.2.1.Правила Checkstyle [#504981]

--- src/test/java/ru/job4j/checkstyle/Broken.java
+++ src/test/java/ru/job4j/checkstyle/Broken.java
@@ -7,15 +7,15 @@ public class Broken {
     private String name;
     private String surname;
 
-    Broken() { }
+    public Broken() { }
 
-    void echo() { }
+    public void echo() { }
 
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e) { }
+    public void method(int a, int b, int c, int d, int e) { }
 }
