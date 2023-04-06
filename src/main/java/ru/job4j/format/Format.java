d2a2795771b365075b2d3d9dbea924a9658c62f8
Popov-Ruslan <ruslan.popov87@gmail.com>
2023-04-06 08:16:56 +0300

    1.2.4.9. Форматирование данных. Методы format() и printf() [#504975]

---
+++ src/main/java/ru/job4j/format/Format.java
@@ -0,0 +1,23 @@
+package ru.job4j.format;
+
+import java.util.Calendar;
+
+public class Format {
+
+    public static void getGreeting(String name, int age) {
+        System.out.println(String.format("Привет, я %s, мой возраст %d", name, age));
+    }
+
+    public static void getGreetingAndTime(String name) {
+        Calendar instance = Calendar.getInstance();
+        System.out.println(String.format("Привет, %s! Текущая дата: %tF, текущее время: %tT", name, instance, instance));
+    }
+
+    public static void main(String[] args) {
+        getGreeting("Руслан", 33);
+        System.out.format("Строка1%nСтрока2%n");
+        getGreetingAndTime("Руслан");
+        System.out.println(String.format("Задаем число 0.99 - %.2f", 0.99));
+        System.out.println(String.format("%1$,+016.2f", 5000000.0000));
+    }
+}
